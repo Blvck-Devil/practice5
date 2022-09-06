f2bffeac0f5cbd3f8efd6d301ef63ace8706f0cd
Blvck-Devil <112816848+Blvck-Devil@users.noreply.github.com>
2022-09-06 08:41:49 +0000

    Calculation

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+//input 3 integers and print the largest ad the smallest of the 3 numbers entered.
+import java.util.*;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int num,min,max;
+    
+    System.out.println("Enter Three Numbers:");
+    Scanner n=new Scanner(System.in);
+
+    System.out.println("Enter 1st Number:");
+    int num1=n.nextInt();
+     System.out.println("Enter 2nd Number:");
+    int num2=n.nextInt();
+     System.out.println("Enter 3rd Number:");
+    int num3=n.nextInt();
+    System.out.println("");
+
+    int x=Math.max(num1,num2);
+    x=Math.max(x,num3);
+
+    int y=Math.min(num1,num2);
+    y=Math.min(y,num3);
+    
+    System.out.println("Largest number is:"+x);
+    System.out.println("Smallest number is:"+y);
   }
 }
